5a046e6906d9b5325cf62320975db6d37295821a
Jadi Josh <meyassu@gmu.edu>
2023-09-14 14:51:57 -0400

    improving basic sniff procedure

--- ae/sniff.py
+++ ae/sniff.py
@@ -1,18 +1,30 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP
 
 def packet_callback(packet):
-    # Print out packet summary
-    print(packet.summary())
-    # You can organize the data into a 2D array here as per your requirements
-    packets_2d_array.append([packet.time, packet.summary()])
+    # Check if the packet has the IP layer
+    if IP in packet:
+        src_ip = packet[IP].src
+        if src_ip not in packets_dict:
+            packets_dict[src_ip] = []
+
+        # Append the packet summary (or the whole packet if needed) to the list for the source IP
+        packets_dict[src_ip].append(packet.summary())
 
 if __name__ == "__main__":
-    packets_2d_array = []
+    packets_dict = {}
     
     print("Starting packet capture...")
-    # Capture 10 packets for demo purposes (you can adjust this number as needed)
-    sniff(count=10, prn=packet_callback)
-    
+    # Capture 50 packets for demo purposes (you can adjust this number as needed)
+    sniff(count=50, prn=packet_callback)
+
+    # Convert dictionary to 2D array
+    packets_2d_array = []
+    for src_ip, packets in packets_dict.items():
+        packets_2d_array.append([src_ip] + packets)
+
     # Print the 2D array
-    for pkt in packets_2d_array:
-        print(pkt)
+    for block in packets_2d_array:
+        print("Source IP:", block[0])
+        for pkt in block[1:]:
+            print("\t", pkt)
+        print("-----------------------")
